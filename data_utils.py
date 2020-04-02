31c7ba39a084203784bcc760f3d46175e332e16b
Kshitij Gupta <kshit@DESKTOP-N4PDMRK.localdomain>
2020-04-01 23:24:25 -0400

    Made changes to take average across bpe tokens

--- data_utils.py
+++ data_utils.py
@@ -92,13 +92,14 @@ def convert_examples_to_features(
             logger.info("Writing example %d of %d", eid, len(examples))
         tokens = []
         label_ids = []
+
         for (word, label) in zip(example.words, example.labels):
             # a word might be split into multiple tokens
             word_tokens = tokenizer.tokenize(word)
             tokens += list(word_tokens)
             # Use the real label id for the first token of the word,
             # and padding ids for the remaining tokens
-            label_ids += [label] + [pad_token_label_id] * (len(word_tokens)-1)
+            label_ids += [label] + [-300] * (len(word_tokens)-1)
         sent_lens.append(len(tokens))
         
         # Account for [CLS] and [SEP] with "- 2" and with "- 3" for RoBERTa.
